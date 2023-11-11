ee5a98a6051e3a7121be31e1d2d70a14b4dc9a94
mseonkim <mseonkim25@gmail.com>
2023-11-12 01:37:55 +0900

    Add smoke property & Disable test smoke feature

--- Runtime/Scripts/LocalVolumetricFog/SmokeVolume.cs
+++ Runtime/Scripts/LocalVolumetricFog/SmokeVolume.cs
@@ -37,15 +37,25 @@ namespace UniversalForwardPlusVolumetric
             public readonly static int _InteractiveSmokeMask = Shader.PropertyToID("_InteractiveSmokeMask");
             public readonly static int _SmokeVolumeViewProjM = Shader.PropertyToID("_SmokeVolumeViewProjM");
             public readonly static int _SmokeCameraFarPlane = Shader.PropertyToID("_SmokeCameraFarPlane");
+            public readonly static int _SmokeVolumeParams0 = Shader.PropertyToID("_SmokeVolumeParams0");
+            public readonly static int _SmokeVolumeParams1 = Shader.PropertyToID("_SmokeVolumeParams1");
         }
 
         [Header("Smoke")]
-        public Vector3 speed;
-        public RenderTexture smokeMaskRT;
+        public float windSpeed = 1f;
+        public Vector2 windDirection = new Vector2(1f, 0f);
+        public float counterFlowSpeed = 1f;
+        public float tiling = 0.5f;
+        public float detailNoiseTiling = 0.85f;
+        public float flatten = 2f;
         
-        // Make sure that you Enabled the camera component & set as overlay mode so you can retrieve VP matrix,
-        // but don't link to camera stack to prevent rendering. 
-        public Camera maskCamera;
+        /*
+         * [Enable once the interactive smoke is fixed]
+         * public RenderTexture smokeMaskRT;
+         * // Make sure that you Enabled the camera component & set as overlay mode so you can retrieve VP matrix,
+         * // but don't link to camera stack to prevent rendering. 
+         * public Camera maskCamera;
+        */
 
         void Awake()
         {
@@ -55,27 +65,33 @@ namespace UniversalForwardPlusVolumetric
         public override void SetComputeShaderProperties(CommandBuffer cmd, ComputeShader cs, int kernel)
         {
             cmd.SetComputeTextureParam(cs, kernel, IDs._MaskTexture, mask);
-            cmd.SetComputeTextureParam(cs, kernel, IDs._InteractiveSmokeMask, smokeMaskRT);
-            cmd.SetComputeFloatParam(cs, IDs._SmokeCameraFarPlane, maskCamera.farClipPlane);
+            var normalizedWindDirection = windDirection.normalized;
+            cmd.SetComputeVectorParam(cs, IDs._SmokeVolumeParams0, new Vector4(windSpeed, counterFlowSpeed, normalizedWindDirection.x, normalizedWindDirection.y));
+            cmd.SetComputeVectorParam(cs, IDs._SmokeVolumeParams1, new Vector4(tiling, detailNoiseTiling, flatten, 0));
+            // cmd.SetComputeTextureParam(cs, kernel, IDs._InteractiveSmokeMask, smokeMaskRT);
+            // cmd.SetComputeFloatParam(cs, IDs._SmokeCameraFarPlane, maskCamera.farClipPlane);
         }
 
         public override bool UpdateRenderTextureIfNeeded(ScriptableRenderContext context, CommandBuffer cmd,  ref RenderingData renderingData)
         {
-            if (maskCamera == null || smokeMaskRT == null)
-                return false;
+            // [Enable once the interactive smoke is fixed]
+            return false;
 
-            var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
-            var drawSettings = RenderingUtils.CreateDrawingSettings(k_ShaderTagId, ref renderingData, SortingCriteria.CommonOpaque);
+            // if (maskCamera == null || smokeMaskRT == null)
+            //     return false;
 
-            CoreUtils.SetRenderTarget(cmd, smokeMaskRT, ClearFlag.Color);
-            cmd.SetGlobalMatrix(IDs._SmokeVolumeViewProjM, maskCamera.previousViewProjectionMatrix);
-            context.ExecuteCommandBuffer(cmd);
-            cmd.Clear();
-            context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref filteringSettings);
-            context.ExecuteCommandBuffer(cmd);
-            cmd.Clear();
+            // var filteringSettings = new FilteringSettings(RenderQueueRange.opaque);
+            // var drawSettings = RenderingUtils.CreateDrawingSettings(k_ShaderTagId, ref renderingData, SortingCriteria.CommonOpaque);
 
-            return true;
+            // CoreUtils.SetRenderTarget(cmd, smokeMaskRT, ClearFlag.Color);
+            // cmd.SetGlobalMatrix(IDs._SmokeVolumeViewProjM, maskCamera.previousViewProjectionMatrix);
+            // context.ExecuteCommandBuffer(cmd);
+            // cmd.Clear();
+            // context.DrawRenderers(renderingData.cullResults, ref drawSettings, ref filteringSettings);
+            // context.ExecuteCommandBuffer(cmd);
+            // cmd.Clear();
+
+            // return true;
         }
 
     }
